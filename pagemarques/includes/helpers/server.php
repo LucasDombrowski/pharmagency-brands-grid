b6934086bbb76e924bf0e164e401203785d8a590
Lucas Dombrowski <lucas.dombrowski.pro@gmail.com>
2024-06-25 15:27:35 +0200

    Updating WP Plugin

--- pagemarques/includes/helpers/server.php
+++ pagemarques/includes/helpers/server.php
@@ -1,8 +1,28 @@
 <?php
 
-function pagemarques_get_domain(){
-    return $_SERVER["HTTP_HOST"];
+function pagemarques_get_domain() {
+    // Get the host from the server variable
+    $host = $_SERVER["HTTP_HOST"];
+    
+    // Split the host by dots
+    $parts = explode('.', $host);
+
+    // Determine the number of parts
+    $numParts = count($parts);
+
+    // Handle cases with subdomains
+    if ($numParts > 2) {
+        // If it has more than two parts, return the last two parts as the main domain
+        $mainDomain = $parts[$numParts - 2] . '.' . $parts[$numParts - 1];
+    } else {
+        // If it has only two parts, return it as is
+        $mainDomain = $host;
+    }
+
+    return $mainDomain;
 }
+
+
 
 function pagemarques_get_api_server(){
     return "https://data.catalogues-pharmagency.fr/api";
--- pagemarques/react-app/src/App.js
+++ pagemarques/react-app/src/App.js
@@ -7,8 +7,14 @@ function App() {
   console.log(data);
   return (
     <div className='w-full'>
-      <CategoryGrid
-      {...data}/>
+      {(data.data && data.data.categories.length > 0) ? <CategoryGrid
+        {...data} /> : <div className='py-6 px-3'>
+        <div className='flex flex-col items-center text-center'>
+          <h2>À venir !</h2>
+          <p>Les marques disponibles dans notre pharmacie apparaîtront bientôt ici.</p>
+          <p>Merci pour votre patience.</p>
+        </div>
+      </div>}
     </div>
   );
 }
